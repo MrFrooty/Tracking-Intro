88a7f6b8d6e978fee30d9874d18f1dfefe77f6bb
freddy <fredsong99@gmail.com>
2023-04-25 13:08:45 -0700

    Siphon data from CSV into features and targets.

--- combinedtrack.py
+++ combinedtrack.py
@@ -1,7 +1,4 @@
 #EACH RUN USING THE DIFFERENT CLASS NAME ADDS COORDINATES THAT CORRESPOND WITH THAT EMOTION
-
-
-
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -40,7 +37,9 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidenc
                                   mp_drawing.DrawingSpec(color=(0,0,255),thickness=5,circle_radius=4),
                                   mp_drawing.DrawingSpec(color=(0,0,240),thickness=5,circle_radius=2))
         
+        #read in facial data here 
         try:
+            class_name = "Sad"
             pose = results.pose_landmarks.landmark
             pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
             
@@ -49,7 +48,7 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidenc
             
             #combine the coordinates of face and pose into a single row
             row = pose_row + face_row
-            row.insert(0, "Happy") #find out a way to implement different emotions for different rows
+            row.insert(0, class_name) #find out a way to implement different emotions for different rows
             
             #write into csv file
             with open('coords.csv', mode='a', newline='') as f: #'a' means append and 'w' means write
